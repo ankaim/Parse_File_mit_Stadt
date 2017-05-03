2f8be2d35bb1b17315c817a0c3840a10858507de
ankaim <kaim@mail.ru>
2017-05-03 22:57:54 +0200

    Добавленны регионы

--- src/ScanSave.java
+++ src/ScanSave.java
@@ -1,6 +1,7 @@
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import java.io.File;
 import java.io.FileWriter;
@@ -9,9 +10,10 @@ import java.io.IOException;
 public class ScanSave {
     static Document doc;
     static FileWriter fileWriter;
+    static String region;
     public static void main(String[] args) throws IOException, InterruptedException {
         fileWriter = new FileWriter(new File("listStad.txt"), true);
-        for (int i = 5000; i <10000; i++) {//23721
+        for (int i = 1; i <3000; i++) {//23721
             try {
                 doc = Jsoup.connect("http://pogodnik.com/" + i).get();
             } catch (Exception e) {
@@ -21,17 +23,23 @@ public class ScanSave {
             Elements ukr = doc.getElementsByAttributeValue("class", "mci-cont");
             String[] mas = temp.text().split(" ");
             String[] mas2 = ukr.text().split(" ");
-            //System.out.println(Arrays.toString(mas2));
-            //\System.out.println(mas2[mas2.length - 1]);
+            try {
+                Element reg = ukr.get(0);
+                region = reg.getElementsByTag("h4").text();
+            }catch (Exception e){
+                continue;
+            }
+
             if (!mas2[mas2.length - 1].equals("Украина")) {
                 int indFin = 0;
                 for (int j = 0; j < mas.length; j++) {
                     if (mas[j].equals("Прогноз")) indFin = j;
                 }
-                fileWriter.write(i + " ");
+                fileWriter.write(i + " | ");
                 for (int j = 7; j < indFin; j++) {
                     fileWriter.write(mas[j] + " ");
                 }
+                fileWriter.write(" | "+region);
                 fileWriter.write("\n");
                 fileWriter.flush();
             }
